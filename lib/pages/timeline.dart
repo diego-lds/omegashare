56fb198c48949e4478ef67623a4755eaf8857373
Diego <diegolopes087@gmail.com>
2020-04-20 19:38:14 -0300

    Buscando dados da firestore

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:omegashare/pages/progress.dart';
 import 'header.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,11 +12,30 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, titleText: 'Omega Share'),
-      body: linearProgress(),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: usersRef.snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return circularProgress();
+            }
+            final children = snapshot.data.documents
+                .map((doc) => Text(doc['username']))
+                .toList();
+            return Container(
+              child: ListView(children: children),
+            );
+          }),
     );
   }
 }
